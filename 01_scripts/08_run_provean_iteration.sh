388ef4193ffc26ae6f8de6e3331cb3dd71cdb9a3
ubuntu <quentinrougemont@orange.fr>
2018-02-07 18:08:28 -0500

    add parallelisation script

---
+++ 01_scripts/08_run_provean_iteration.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+input_file=$1
+input_basename=$(basename "$input_file")
+path="/home/qurou/software/provean-1.1.5/"
+
+output_folder="07_provean_scores"
+output_sss="$output_folder"/${input_basename%.fasta}.sss
+
+"$path"/provean.sh \
+    -q $input_file \
+    -v ${input_file%.fasta}.var \
+    --save_supporting_set "$output_sss"  >> allout.txt 2>&1 
